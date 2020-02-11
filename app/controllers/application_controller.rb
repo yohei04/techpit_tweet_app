bbf568e2eaf30ec67da944de1357490aa3e943e3
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-02-11 16:17:48 +0900

    サインイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    static_pages_home_path
+  end
+
+
+  # deviseのストロングパラメーターを編集
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :user_name]) 
   end
