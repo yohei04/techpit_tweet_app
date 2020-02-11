fb238049bc50baa7057965d3d0517c0b6e30f2ba
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-02-11 15:51:13 +0900

    サインアップ機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: "users/sessions",
   }
+
+  devise_scope :user do
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+  end
+
   get 'static_pages/home'
 end
